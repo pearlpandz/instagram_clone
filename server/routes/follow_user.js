fd3808fced171f8182232bab3a9d35e914b4f4a8
nandhini <rnandhini096@hotmail.com>
2018-10-25 11:58:34 +0530

    followers

--- server/routes/follow_user.js
+++ server/routes/follow_user.js
@@ -5,39 +5,19 @@ const users= require('../models/user'); //create new post schema
 
 
 exports.follow = function (req, res) {
- //  console.log(req.body.user_id);
-   //console.log(req.body.follower_id);
+  console.log(req.body.user_id);
+   console.log(req.body.follower_id);
 
-//let user_id = req.body.user_id;
-var user_id = {"_id": req.body.user_id};
-let follower_id = req.body.follower_id;
-var options = {new: true};
-// users.findOneAndUpdate(user_id ,{$addToSet:{followers:follower_id}}, options, 
-    
-//     function(err, post) {
-
-//     if (err) {
-//       res.send(err);
-//     }
-//     else {
-//         users.find({ 'user': follower_id }).updateOne({
-//             $addToSet: {
-//                 following: user_id
-//             }
-//         })
-//     }
-
-//   }); 
-
+let user_id = req.body.user_id;
 let bulk = users.collection.initializeUnorderedBulkOp();
  
-bulk.find({ 'user': user_id}).upsert().updateOne({
+bulk.find({ 'users': user_id}).upsert().updateOne({
     $addToSet: {
         following: follower_id
     }
 });
 
-bulk.find({ 'user': follower_id}).upsert().updateOne({
+bulk.find({ 'users': follower_id}).upsert().updateOne({
     $addToSet: {
         followers: user_id
     }
@@ -54,9 +34,7 @@ bulk.execute(function(err, doc) {
         'state': true,
         'msg': 'Followed'
     })
-})
-
-
+});
 
    };
 
