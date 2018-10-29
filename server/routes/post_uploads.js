2a21f03be24e9a0a8dd1e75ac7ae996136a0e28c
pearlpandz <muthu@appoets.com>
2018-10-29 13:23:36 +0530

    post_uploads issue fixed

--- server/routes/post_uploads.js
+++ server/routes/post_uploads.js
@@ -6,12 +6,10 @@ const Posts = require('../models/index'); //create new post schema
 exports.upload = function(req,res){
     var receiveArrayFiles = req.files;
     var regularpath = [];
-
+    console.log(receiveArrayFiles);
     for (var i =0; i < receiveArrayFiles.length; i++ ) {
-      // console.log('--- ',receiveArrayFiles[i])
       regularpath.push(req.protocol + "://" + req.get('host') + '/' + receiveArrayFiles[i].path);
     }
-    // console.log("array regular", regularpath);
 
     let posts = new Posts ({
       sampleFile: regularpath,
@@ -20,15 +18,13 @@ exports.upload = function(req,res){
     let mangooseid = req.body._id;
   
     var query = {"_id": mangooseid};
-    var update = {sampleFile: regularpath};
     var options = {new: true};
-    Posts.findOneAndUpdate(query, {password: 0} ,{$set:{sampleFile:regularpath}}, options, function(err, post) {
+    Posts.findOneAndUpdate(query, {$set:{sampleFile:regularpath}}, options, function(err, post) {
       console.log('000000 = ',err, post)
       if (err) {
         console.log('got an error');
       }
       else {
-        console.log(post);
         res.send(post); 
       }
     
