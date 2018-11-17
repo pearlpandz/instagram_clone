cc7f94425af42d915d04aee1fc45ff13b4990390
nandhini <rnandhini096@hotmail.com>
2018-11-17 18:51:14 +0530

    password work api done by bcrypt

--- server/routes/pwd_change.js
+++ server/routes/pwd_change.js
@@ -5,41 +5,54 @@ const users = require('../models/user'); //create new post schema
 const posts =  require('../models/index') 
 var bcrypt = require('bcrypt');
 let salt= bcrypt.genSaltSync(8);
-exports.changepwd  = function (req, res, next) {
-//  oldpwd = req.body.oldpwd;
-//  newpwd = req.body.newpwd;
-// password = {'password':req.body.password}
-// oldpass=  {'password': req.body.oldpass};
-// newpass= {'password':req.body.newpass}
-//  emails = req.body.emails;
- users.findOne({name: req.body.name}, function(err, user){
-    //    console.log(oldpass);
-    //    console.log(newpass);
-    //   console.log(user.password);
-   if ( bcrypt.compare(user.password == req.body.oldpass)){
-          res.send('oldpass crct')
-      }
-     else{
-               res.send("incorrect");
-     }})
-            // users.findOneAndUpdate( {'password': req.body.oldpass} , {
-            //         $set: {
-            //             password: req.body.newpass
-            //         }
-            //     }, {new: true}, function(err1, user1) {
-                    
-            //         if(err1){
-            //             res.send(err1);
-            //         }
-            //         else{
-            //             res.send(user1);
-            //         }
-            //     }
-            //     ) 
-                  
 
-     
-//   }
-     
-        
+    exports.changepwd = function(req, res) {
+    //old password for bcrypt compare 
+    //new password for set
+    
+    let newpass = bcrypt.hashSync(req.body.newpass, salt);
+    users.findOne({ name: req.body.name }, function(err, user) {
+    
+    bcrypt.compare(req.body.oldpass, user.password, function(err, pass) {
+    if (err) {
+    res.send(err);
+    } else {
+    console.log(pass);
+    if (pass == true) {
+    users.findOneAndUpdate({ name: req.body.name }, {
+    $set: {
+    password: newpass
+    }
+    }, { new: true }, function(err1, user1) {
+    
+    if (err1) {
+    res.send({
+    status: false,
+    data: err1
+    });
+    }
+    if (user1) {
+    res.send({
+    status: true,
+    msg: 'New password updated',
+    data: user1
+    });
+    } else {
+    res.send({
+    status: true,
+    msg: 'data not return',
+    data: user1
+    });
+    }
+    })
+    } else {
+    res.json({
+    status: pass,
+    msg: 'The is your old password'
+    })
+    }
+    }
+    })
+    })
     };
+    
